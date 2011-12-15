f89cc94920d34c338f9a155555be564f028df4e8
Leon N. Maurer <leon.maurer@gmail.com>
2011-12-15 09:19:40 -0600

    Added some documentation to the functions for use in the code frame.

--- qubit_control.py
+++ qubit_control.py
@@ -967,29 +967,36 @@ if __name__ == "__main__":
 
   #the following functions will come in useful when evaluating code in code frame
   def setValue(nameString, newValue):
+    """Sets the value of the ViewValue named nameString to newValue."""
     gui.setValue(nameString, newValue)
     
   def setTime(nameString, newTime):
+    """Sets the time of the ViewTime name nameString to newTime."""
     gui.setTime(nameString, newTime)
     
   def addTime(name, time):
+    """Makes and adds a ViewTime with the the given name and time."""
     gui.addTime(name=name, time=time)
     
   def deleteTime(name):
+    """Deletes the ViewTime with the given name."""
     gui.deleteTime(name=name)
     
   def setDurationValue(traceName, durationName, valueName):
+    """Sets the assocViewValue of the ViewDuration named durationName, in the trace name traceName, to the ViewValue named valueName."""
     trace = gui.traceNamed(traceName)
     duration = trace.durationNamed(durationName)
     value = gui.valueNamed(valueName)
     duration.setViewValue(value)
     
   def mergeValues(valueNameA, valueNameB):
+    """Merges the two named ViewValues."""
     valueA = gui.valueNamed(valueNameA)
     valueB = gui.valueNamed(valueNameB)
     valueB.merge(valueA)
     
   def setDurationName(traceName, startTimeName, newDurationName):
+    """Sets the name of the ViewDuration with the startTime named startTimeName, in the trace named traceName, to newDurationName."""
     trace = gui.traceNamed(traceName)
     time = gui.timeNamed(startTimeName)
     trace.durationStartingAt(time).setName(newDurationName)
